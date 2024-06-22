540902b9e5a848fac9d0e3f682565d566b489e2f
Jacob Jerrell <jacob@jjerrell.dev>
2024-06-22 12:53:49 -0500

    Loosely integrates the Category DB into the Android app for testing

--- androidApp/src/main/java/dev/jjerrell/root/records/android/MainActivity.kt
+++ androidApp/src/main/java/dev/jjerrell/root/records/android/MainActivity.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.jjerrell.root.records.Greeting
+import dev.jjerrell.root.records.db.DriverFactory
+import dev.jjerrell.root.records.db.createDatabase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val database = createDatabase(DriverFactory(this))
+        val categoryQueries = database.categoryQueries
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    LazyColumn {
+                        item {
+                            GreetingView(Greeting().greet())
+                        }
+                        items(items = categoryQueries.selectAll().executeAsList()) {
+                            Text(text = it.name)
+                        }
+                    }
                 }
             }
         }
