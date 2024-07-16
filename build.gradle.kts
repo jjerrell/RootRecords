1968045f1c0e15ddc53d9af3f3397ab191a9309c
Jacob Jerrell <jacob@jjerrell.dev>
2024-07-15 19:54:27 -0500

    Add and run spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,19 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.sqlDelight).apply(false)
+    alias(libs.plugins.spotless)
 }
 
+spotless {
+    kotlin {
+        ktlint("0.50.0")
+            .userData(
+                mapOf(
+                    "trailing-comma" to "disabled"
+                )
+            )
+        ratchetFrom("origin/main")
+        target("src/*/kotlin/**/*.kt")
+        targetExclude("${layout.buildDirectory}/**/*.kt")
+    }
+}
