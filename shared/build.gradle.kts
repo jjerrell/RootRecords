bd020d1fe350bd14f3ff327e178a295eda4cff48
Jacob Jerrell <jacob@jjerrell.dev>
2024-07-27 08:24:25 -0500

    Address Gradle function deprecation warning

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,14 +5,7 @@ plugins {
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-    }
-    
+    androidTarget()
     listOf(
         iosX64(),
         iosArm64(),
