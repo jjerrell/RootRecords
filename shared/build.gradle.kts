f5bc12d0d09652e550ff168ee7749d65d04605ea
Jacob Jerrell <jacob@jjerrell.dev>
2024-07-27 07:16:12 -0500

    Java Upgrade 1.8->17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -8,7 +8,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "17"
             }
         }
     }
@@ -47,8 +47,8 @@ android {
         minSdk = 29
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
