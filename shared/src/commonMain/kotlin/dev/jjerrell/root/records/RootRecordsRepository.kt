db159655da23903b99017f8bbd44274f8ca7847a
Jacob Jerrell <jacob@jjerrell.dev>
2024-07-28 09:22:51 -0500

    Adds conversions and eliminates SQLDelight `taskQueries`

--- shared/src/commonMain/kotlin/dev/jjerrell/root/records/RootRecordsRepository.kt
+++ shared/src/commonMain/kotlin/dev/jjerrell/root/records/RootRecordsRepository.kt
@@ -4,51 +4,46 @@ import dev.jjerrell.root.records.db.CategoryEntity
 import dev.jjerrell.root.records.db.DriverFactory
 import dev.jjerrell.root.records.db.TaskEntity
 import dev.jjerrell.root.records.db.createDatabase
+import dev.jjerrell.root.records.db.createRoomDatabase
 import dev.jjerrell.root.records.model.Category
 import dev.jjerrell.root.records.model.Task
+import dev.jjerrell.root.records.model.db.CategoryDbEntity
+import dev.jjerrell.root.records.model.db.TaskDbEntity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.datetime.Instant
 
 class RootRecordsRepository(databaseDriverFactory: DriverFactory) {
     private val database = createDatabase(databaseDriverFactory)
     private val categoryQueries = database.categoryEntityQueries
-    private val taskQueries = database.taskEntityQueries
+    private val roomDb = createRoomDatabase(databaseDriverFactory)
 
-    fun getAllTasks(): List<Task> {
-        return taskQueries
-            .selectAll()
-            .executeAsList()
-            .map { taskEntity ->
-                taskEntity.toTask { categoryId ->
-                    categoryId?.let {
-                        categoryQueries.selectById(it)
-                            .executeAsOne()
-                    }
-                }
-            }
+    suspend fun getAllTasks(): List<Task> {
+        return roomDb
+            .taskDao()
+            .getAllTasks()
+            .map { it.map(TaskDbEntity::toTask) }
+            .first()
     }
 
-    fun getTaskById(id: String): Task {
-        return taskQueries.selectTaskById(TaskEntity.Id(id))
-            .executeAsOne()
-            .toTask { categoryId ->
-                categoryId?.let {
-                    categoryQueries.selectById(it)
-                        .executeAsOne()
-                }
-            }
+    suspend fun getTaskById(id: String): Task {
+        return roomDb
+            .taskDao()
+            .getTaskById(id)
+            .map { it.toTask() }
+            .first()
     }
 
-    fun insertTask(item: Task) {
-        return taskQueries.insertFullTaskObject(item.toTaskEntity())
+    suspend fun insertTask(item: Task) {
+        return roomDb
+            .taskDao()
+            .insertTask(item.toTaskDbEntity())
     }
 
-    fun updateTask(item: Task) {
-        return taskQueries.updateByValues(
-            name = item.name,
-            description = item.description,
-            date = item.timestamp.epochSeconds,
-            id = TaskEntity.Id(item.id)
-        )
+    suspend fun updateTask(item: Task) {
+        return roomDb
+            .taskDao()
+            .updateTask(item.toTaskDbEntity())
     }
 
     fun getCategories(): List<Category> {
@@ -78,6 +73,7 @@ class RootRecordsRepository(databaseDriverFactory: DriverFactory) {
     }
 }
 
+//region Deprecated SQLDelight
 private fun TaskEntity.toTask(
     categoryQuery: (CategoryEntity.Id?) -> CategoryEntity?
 ): Task = Task(
@@ -111,3 +107,34 @@ private fun Category.toCategoryEntity(): CategoryEntity = CategoryEntity(
     color = color
 
 )
+//endregion
+
+//region Room
+private fun TaskDbEntity.toTask(): Task = Task(
+    id = id,
+    name = name,
+    description = description,
+    timestamp = Instant.fromEpochSeconds(date),
+    category = category?.toCategory()
+)
+
+private fun Task.toTaskDbEntity(): TaskDbEntity = TaskDbEntity(
+    id = id,
+    name = name,
+    description = description,
+    date = timestamp.epochSeconds,
+    category = category?.toCategoryDbEntity()
+)
+
+private fun CategoryDbEntity.toCategory(): Category = Category(
+    id = id,
+    name = name,
+    color = color
+)
+
+private fun Category.toCategoryDbEntity(): CategoryDbEntity = CategoryDbEntity(
+    id = id,
+    name = name,
+    color = color
+)
+//endregion
