ccc265d6d1b3e9f7c38da8a50e9534e2f88cf95d
Jacob Jerrell <jacob@jjerrell.dev>
2024-08-08 19:09:10 -0500

    Adds and configures spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,11 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    afterEvaluate {
+        project.apply(rootProject.projectDir.path + "/build-support/spotless.gradle")
+    }
 }
