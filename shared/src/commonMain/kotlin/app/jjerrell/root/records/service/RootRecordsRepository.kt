f77efc557726b2d4a082d2acf72b5c16d9b47141
Jacob Jerrell <jacob@jjerrell.dev>
2024-08-16 20:29:16 -0500

    Implements repository methods for Task/TaskEntity

--- shared/src/commonMain/kotlin/app/jjerrell/root/records/service/RootRecordsRepository.kt
+++ shared/src/commonMain/kotlin/app/jjerrell/root/records/service/RootRecordsRepository.kt
@@ -4,6 +4,9 @@ import app.jjerrell.root.records.service.model.Category
 import app.jjerrell.root.records.db.DatabaseFactory
 import app.jjerrell.root.records.db.createDatabase
 import app.jjerrell.root.records.db.entity.CategoryEntity
+import app.jjerrell.root.records.db.entity.TaskEntity
+import app.jjerrell.root.records.db.entity.TaskWithCategory
+import app.jjerrell.root.records.service.model.Task
 import kotlinx.coroutines.flow.firstOrNull
 
 class RootRecordsRepository(
@@ -11,6 +14,7 @@ class RootRecordsRepository(
 ) {
     private val db = createDatabase(factory)
 
+    //region Category
     suspend fun insertCategory(category: Category) {
         db.categoryDao()
             .insertCategory(category.toEntity())
@@ -34,6 +38,33 @@ class RootRecordsRepository(
             .firstOrNull()
             ?.toModel()
     }
+    //endregion
+
+    //region Task
+    suspend fun insertTask(task: Task) {
+        db.taskDao()
+            .insertTask(task.toEntity())
+    }
+
+    suspend fun updateTask(task: Task) {
+        db.taskDao()
+            .updateTask(task.toEntity())
+    }
+
+    suspend fun getTasks(): List<Task>? {
+        return db.taskDao()
+            .getAllTasks()
+            .firstOrNull()
+            ?.map { it.toModel() }
+    }
+
+    suspend fun getTaskById(id: Int): Task? {
+        return db.taskDao()
+            .getTaskById(id)
+            .firstOrNull()
+            ?.toModel()
+    }
+    //endregion
 }
 
 private fun Category.toEntity() = CategoryEntity(
@@ -45,4 +76,19 @@ private fun CategoryEntity.toModel() = Category(
     id = id,
     name = name,
     color = color
+)
+
+private fun Task.toEntity() = TaskEntity(
+    title = title,
+    description = description,
+    isCompleted = isCompleted,
+    categoryId = categoryId?.id
+)
+
+private fun TaskWithCategory.toModel() = Task(
+    id = task.id,
+    title = task.title,
+    description = task.description,
+    isCompleted = task.isCompleted,
+    categoryId = category?.toModel()
 )
