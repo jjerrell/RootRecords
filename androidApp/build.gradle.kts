1edc2e08ff400bf96d140ef573bb8c7a1985516f
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-04 20:09:07 -0500

    Increase memory allowances. Expand on build types

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,7 +16,18 @@ android {
     }
     buildFeatures { compose = true }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-    buildTypes { getByName("release") { isMinifyEnabled = false } }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
