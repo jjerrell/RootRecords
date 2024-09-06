4b97c34a7cef5ece6768c024f6c0eeeb10779734
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-05 22:19:28 -0500

    Add completion indication to task list items

--- androidApp/feature-task/src/main/java/app/jjerrell/root/records/android/feature/task/list/TaskListItem.kt
+++ androidApp/feature-task/src/main/java/app/jjerrell/root/records/android/feature/task/list/TaskListItem.kt
@@ -18,15 +18,19 @@
 package app.jjerrell.root.records.android.feature.task.list
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,11 +52,18 @@ internal fun TaskListItem(modifier: Modifier = Modifier, task: Task, onTaskClick
                 ?: CardDefaults.cardColors(),
         onClick = onTaskClick
     ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            Text(text = task.title)
-            task.description
-                .takeUnless { it.isBlank() }
-                ?.let { Text(text = it, style = MaterialTheme.typography.bodySmall) }
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Checkbox(checked = task.isCompleted, onCheckedChange = null)
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(text = task.title)
+                task.description
+                    .takeUnless { it.isBlank() }
+                    ?.let { Text(text = it, style = MaterialTheme.typography.bodySmall) }
+            }
         }
     }
 }
