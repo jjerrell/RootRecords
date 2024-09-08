8f6691b376033c0cd2e434b90751cebbec6f336a
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-08 11:21:20 -0500

    Encapsulate the duplicated SwipeToDismiss logic in a component and integrate

--- androidApp/feature-category/src/main/java/app/jjerrell/root/records/android/feature/category/list/CategoryListView.kt
+++ androidApp/feature-category/src/main/java/app/jjerrell/root/records/android/feature/category/list/CategoryListView.kt
@@ -24,21 +24,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import app.jjerrell.root.records.android.ui.core.component.RootSwipeToDismiss
 import app.jjerrell.root.records.android.ui.theme.RootRecordsTheme
 import app.jjerrell.root.records.service.model.Category
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun CategoryListView(
     modifier: Modifier = Modifier,
@@ -52,22 +48,11 @@ internal fun CategoryListView(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(items = categories) { category: Category ->
-            val swipeState = rememberSwipeToDismissBoxState()
-            when (swipeState.currentValue) {
-                SwipeToDismissBoxValue.EndToStart -> {
-                    LaunchedEffect(swipeState) {
+            RootSwipeToDismiss(
+                onSwipeValue = { swipeValue: SwipeToDismissBoxValue ->
+                    if (swipeValue == SwipeToDismissBoxValue.EndToStart) {
                         onCategoryDelete(category.id!!)
-                        swipeState.snapTo(SwipeToDismissBoxValue.Settled)
                     }
-                }
-                else -> {}
-            }
-            SwipeToDismissBox(
-                state = swipeState,
-                modifier = modifier,
-                enableDismissFromStartToEnd = false,
-                backgroundContent = {
-                    // TODO?
                 },
                 content = {
                     CategoryListItem(
--- androidApp/feature-task/src/main/java/app/jjerrell/root/records/android/feature/task/list/TaskListView.kt
+++ androidApp/feature-task/src/main/java/app/jjerrell/root/records/android/feature/task/list/TaskListView.kt
@@ -24,19 +24,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import app.jjerrell.root.records.android.ui.core.component.RootSwipeToDismiss
 import app.jjerrell.root.records.android.ui.theme.RootRecordsTheme
 import app.jjerrell.root.records.service.model.Task
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun TaskListView(
     modifier: Modifier = Modifier,
@@ -50,22 +46,11 @@ internal fun TaskListView(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(items = tasks) { task: Task ->
-            val swipeState = rememberSwipeToDismissBoxState()
-            when (swipeState.currentValue) {
-                SwipeToDismissBoxValue.EndToStart -> {
-                    LaunchedEffect(swipeState) {
+            RootSwipeToDismiss(
+                onSwipeValue = { swipeValue: SwipeToDismissBoxValue ->
+                    if (swipeValue == SwipeToDismissBoxValue.EndToStart) {
                         onTaskDelete(task.id!!)
-                        swipeState.snapTo(SwipeToDismissBoxValue.Settled)
                     }
-                }
-                else -> {}
-            }
-            SwipeToDismissBox(
-                state = swipeState,
-                modifier = modifier,
-                enableDismissFromStartToEnd = false,
-                backgroundContent = {
-                    // TODO?
                 },
                 content = { TaskListItem(task = task, onTaskClick = { onTaskClick(task.id!!) }) }
             )
