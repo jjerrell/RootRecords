5f8ec83e63b948b92a264cd153ea6a44a97bc2e3
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-16 21:11:27 -0500

    Adds a missed import and runs spotless

--- androidApp/src/main/java/app/jjerrell/root/records/android/MainLayoutViewModel.kt
+++ androidApp/src/main/java/app/jjerrell/root/records/android/MainLayoutViewModel.kt
@@ -1,6 +1,25 @@
+/*
+ * RootRecords
+ * Copyright (C) 2024  Jacob Jerrell (@jjerrell)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package app.jjerrell.root.records.android
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import app.jjerrell.root.records.android.ui.theme.DisplayType
 import app.jjerrell.root.records.android.ui.theme.RootDisplays
